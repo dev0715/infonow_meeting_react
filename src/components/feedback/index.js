16bd558e24d389ff72b0a0f4b14027bf77eec5c6
dev0715 <toptal126@outlook.com>
2021-09-08 19:42:11 +0500

    use state issue

--- src/components/feedback/index.js
+++ src/components/feedback/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 
 import { toast } from '../../utils';
 import StarRatings from 'react-star-ratings';
