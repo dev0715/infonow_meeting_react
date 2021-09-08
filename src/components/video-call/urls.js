69b34d86007f2380e63fdfc1221090b8ddf05647
dev0715 <toptal126@outlook.com>
2021-09-08 18:51:21 +0500

    url updated

--- src/components/video-call/urls.js
+++ src/components/video-call/urls.js
@@ -1,8 +1,8 @@
 export const URLs = {
-    main: 'http://192.168.10.104:3901',
+    // main: 'http://192.168.10.104:3901',
     // feedback: 'http://192.168.10.104:3900/meetings/feedback',
     // whiteBoard: 'https://88ecdfca292a.ngrok.io/boards',
-    // main: 'https://meet.meditati.ro/',
+    main: 'https://meet.meditati.ro/',
     feedback: 'https://meet.meditati.ro/api/meetings/feedback',
     whiteBoard: "https://whiteboard.meditati.ro/boards"
 }
