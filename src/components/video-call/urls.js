b3e29e67b8ff1d221e31f12637029ea18b717bcc
dev0715 <toptal126@outlook.com>
2021-10-06 16:55:12 +0500

    fix: Updated the application to use Infonow.ro API

--- src/components/video-call/urls.js
+++ src/components/video-call/urls.js
@@ -2,11 +2,11 @@ export const URLs = {
     // main: 'http://192.168.10.101:3601',
     // rootApi: 'http://192.168.10.102:3600',
     // feedback: 'http://192.168.10.101:3600/meetings/feedback',
-    // whiteBoard: "https://whiteboard.meditati.ro/boards",
-    main: 'https://meet.meditati.ro/',
-    rootApi: 'https://api.meditati.ro/main',
-    feedback: 'https://meet.meditati.ro/api/meetings/feedback',
-    whiteBoard: "https://whiteboard.meditati.ro/boards"
+    // whiteBoard: "https://whiteboard.infonow.ro/boards",
+    main: 'https://meet.infonow.ro/',
+    rootApi: 'https://api.infonow.ro/',
+    feedback: 'https://meet.infonow.ro/api/meetings/feedback',
+    whiteBoard: "https://whiteboard.infonow.ro/boards"
 }
 
 export const getWhiteboardUrl = (meetingId, userId) => `${URLs.whiteBoard}/${meetingId}`;
